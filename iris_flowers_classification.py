0bce8648346b6ca24ae670e4777186cee4983427
Amol <amolvagad@gmail.com>
2017-08-01 22:39:32 -0500

    First commit to just add the libraries and load the dataset and then visualize it using some plots

---
+++ iris_flowers_classification.py
@@ -0,0 +1,85 @@
+
+# Check the versions of libraries
+
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
+
+
+# Load libraries
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+
+
+
+# shape
+print("Shape of dataset =") 
+print(dataset.shape)
+
+
+#taking a peek of the data by looking at the top 15 rows 
+
+print(dataset.head)
+
+
+# descriptions
+print(dataset.describe())
+
+
+
+#class distribution
+print(dataset.groupby('class').size())
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+
+
+
+
+
